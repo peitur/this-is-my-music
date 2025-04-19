f4c67982f1621b4dda5bb34dfc6b2e5098ee8348
peitur <peitur@users.noreply.github.com>
2025-04-19 19:10:08 +0200

    Create app.py

---
+++ timm/app.py
@@ -0,0 +1,18 @@
+#/usr/bin/env python3
+
+import sys
+import re
+import pathlib
+import logging, logging.handlers
+import scapy
+
+from pprint import print
+
+class ListnerBase( object ):
+  def __init__( self, **option ):
+    pass
+
+
+
+if __name__ == "__main__":
+  pass
